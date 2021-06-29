8bf8ec96688f68e6493591db6dd11daf6610726a
Doaa <doaaziadat@gmail.com>
2021-06-29 03:28:37 +0300

    first commit

---
+++ Assets/Scripts/PlayerCont.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCont : MonoBehaviour
+{
+    public GameObject player;
+    public float sensitivity;
+    public float speed;
+    private Animator anim;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W)) {
+
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            anim.SetInteger("run", 1);
+
+
+        }
+        
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            anim.SetInteger("run", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S)) {
+            anim.SetInteger("run", 0);
+
+
+        }
+    }
+    void FixedUpdate()
+    {
+        float rotateHorizontal = Input.GetAxis("Mouse X");
+
+        transform.RotateAround(player.transform.position, -Vector3.up, rotateHorizontal * sensitivity);
+    }
+}
---
+++ Assets/Scripts/Shooting1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting1 : MonoBehaviour
+{
+    public GameObject obj;
+    public Transform posofres;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+
+            Instantiate(obj, posofres.position, posofres.rotation);
+
+        }
+}
+}
